181f9b6381c912d1015bebfb4db4e5e72a38ce8c
omkar <omkar.deshmukh@thinkitive.com>
2025-06-17 12:30:34 +0530

    removed the appliation-dev.yml file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:17
 ENV SPRING_PROFILES_ACTIVE=dev
 COPY target/Mopix-0.0.1-SNAPSHOT.jar app.jar
-EXPOSE 8083
+EXPOSE 8080
 ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/com/mopix/Mopix/MopixApplication.java
+++ src/main/java/com/mopix/Mopix/MopixApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MopixApplication {
 	public static void main(String[] args) {
-		System.out.println("Running Mopix dev");
+		System.out.println("Running Mopix dev_v2");
 		SpringApplication.run(MopixApplication.class, args);
 	}
 
