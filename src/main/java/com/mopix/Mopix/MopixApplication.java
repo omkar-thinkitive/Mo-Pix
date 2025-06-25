b20881371fb3aadf58cb219f09d218028ba2e536
omkar <omkar.deshmukh@thinkitive.com>
2025-06-25 13:12:36 +0530

    change

--- src/main/java/com/mopix/Mopix/MopixApplication.java
+++ src/main/java/com/mopix/Mopix/MopixApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MopixApplication {
 	public static void main(String[] args) {
-		System.out.println("Running Mopix dev_v2");
+		System.out.println("Running Mopix dev_v3");
 		SpringApplication.run(MopixApplication.class, args);
 	}
 
