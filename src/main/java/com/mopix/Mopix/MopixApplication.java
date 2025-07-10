fac8ad32aad8c54dfe50ba980355bb71a060a608
omkar <omkar.deshmukh@thinkitive.com>
2025-07-10 19:59:41 +0530

    delete images

--- src/main/java/com/mopix/Mopix/MopixApplication.java
+++ src/main/java/com/mopix/Mopix/MopixApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MopixApplication {
 	public static void main(String[] args) {
-		System.out.println("Running Mopix dev_v5");
+		System.out.println("Running Mopix dev_v6");
 		SpringApplication.run(MopixApplication.class, args);
 	}
 
